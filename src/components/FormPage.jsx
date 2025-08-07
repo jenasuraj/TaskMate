0eee3044b5f0f0160627b907da87c62a34095c04
jenasuraj <jenasuraj742@gmail.com>
2025-08-07 22:43:16 +0530

    refactor: removed am/pm feature

--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -1,38 +1,10 @@
 import React from 'react'
 
 
-const FormPage = ({setDashboard,setFinalData,formData,setFormData}) => {
+const FormPage = ({setDashboard,setFinalData,formData,setFormData,toBeUpdated}) => {
 
   
-  const checkAmPm = (check)=>{
-    const checkArr = check.split(":")
-    if (checkArr[0]<12) { return "AM" }
-    else { return "PM" }
-  }
-  const timeFixer = (time)=>{
-    let final = null
-    const actual = time.split(":")
-    const timeSlice = parseInt(actual[0])
-    if(timeSlice>12) 
-      { 
-        final = timeSlice-12 
-        const mostFinal = String(final)+":"+actual[1] 
-        return mostFinal 
-      }
-      else{
-        return time
-      }
-  } 
-  const RemoveT = (data) =>{
-    const tobe = data.date
-    const updated = tobe.split('T') 
-    updated[1] = timeFixer(updated[1]) +" "+checkAmPm(updated[1])
-    const final = updated.join(' ')
-    const toBeSent = {...data,date:final}
-    //But in React or good JavaScript practices, we do not directly mutate objects. Instead, 
-    // we use the spread operator to copy the object and update its values immutably.
-    return toBeSent 
-  }
+
 
   const handleChange = (e)=>{
     console.log("handlechange triggered")
@@ -45,7 +17,7 @@ const FormPage = ({setDashboard,setFinalData,formData,setFormData}) => {
   e.preventDefault()  
   if(!formData.name || !formData.description || !formData.title || !formData.date){ return }
   else{
-    setFinalData(prev=>[...prev,RemoveT(formData)])
+    setFinalData(prev=>[...prev,formData])
     setFormData({name:'',description:'',date:'',title:''})
     setDashboard(true)
   }
@@ -58,7 +30,7 @@ const FormPage = ({setDashboard,setFinalData,formData,setFormData}) => {
           <input name='title' type="text" placeholder='Enter title' value={formData.title} className='bg-white py-3 px-10 ' onChange={handleChange}/>
           <input name='description' type="text" placeholder='Enter Description' value={formData.description} className='bg-white py-3 px-10' onChange={handleChange}/>
           <input name='date' type="datetime-local"  className='bg-white py-3 px-10 ' value={formData.date} onChange={handleChange}/>
-          <button className='bg-black text-white border border-gray-500 py-4 px-10 cursor-pointer' type='submit'>Submit</button>
+          <button className='bg-black text-white border border-gray-500 py-4 px-10 cursor-pointer' type='submit'>{toBeUpdated ? 'Update': 'Sub'}</button>
         </form>
       </section>
   )
